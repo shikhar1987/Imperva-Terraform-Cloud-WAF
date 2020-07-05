4ee6b98ceebcc529c663961a771167b1f7907223
shikhar1987 <shikhar_sharma@live.com>
2020-07-06 04:11:10 +1000

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,54 @@
+provider "incapsula" {
+  api_id  = var.api_id
+  api_key = var.api_key
+}
+provider "aws" {
+  region = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+data "aws_route53_zone" "zone" {
+  name = "securitytestingsolutions.com."
+}
+locals {
+  application_information = csvdecode(var.input_file)
+}
+
+resource "incapsula_site" "devops-sites" {
+    count = length(local.application_information)
+
+  domain                 = local.application_information[count.index].domain
+  account_id             = local.application_information[count.index].account_id
+  ref_id                 = "123456"
+  force_ssl              = local.application_information[count.index].force_ssl
+  data_storage_region    = local.application_information[count.index].data_storage_region
+  send_site_setup_emails = local.application_information[count.index].send_site_setup_emails
+  site_ip                = local.application_information[count.index].site_ip
+}
+
+resource "aws_route53_record" "cert-validation-record" {
+
+  depends_on = [incapsula_site.devops-sites]
+  allow_overwrite = true
+  name = data.aws_route53_zone.zone.name
+  type = "TXT"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops-sites[0].domain_verification]
+}
+
+resource "aws_route53_record" "cname-record" {
+    count = length(local.application_information)
+
+  depends_on = [incapsula_site.devops-sites]
+  allow_overwrite = true
+  name = incapsula_site.devops-sites[count.index].domain
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops-sites[count.index].dns_cname_record_value!=null ? incapsula_site.devops-sites[count.index].dns_cname_record_value : 0]
+}
+
+output "CNAME" {
+  value       = aws_route53_record.cert-validation-record.*.records
+}
