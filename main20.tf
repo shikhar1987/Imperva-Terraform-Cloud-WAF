aa4a8a70b8ad206bdd48c144efb7b57735792081
shikhar1987 <shikhar_sharma@live.com>
2020-07-08 03:32:32 +1000

    added main20 file

---
+++ main20.tf
@@ -0,0 +1,315 @@
+provider "incapsula" {
+  api_id  = "xxxx"
+  api_key = "xxxx"
+}
+provider "aws" {
+  region = "ap-southeast-2"
+  access_key = "xxxx"
+  secret_key = "xxxx"
+}
+data "aws_route53_zone" "zone" {
+  name = "securitytestingsolutions.com."
+}
+
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
+  ref_id                 = "123456"
+  force_ssl              = "false"
+  data_storage_region    = "AU"
+}
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  zone_id = data.aws_route53_zone.zone.zone_id
+  ttl = "60"
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
+}
+
