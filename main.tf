19ac06c9e153366f84634eb22cf9948e34860a32
shikhar1987 <shikhar_sharma@live.com>
2020-07-08 03:22:35 +1000

    Example file

--- main.tf
+++ main.tf
@@ -1,54 +1,29 @@
 provider "incapsula" {
-  api_id  = var.api_id
-  api_key = var.api_key
+  api_id  = "xxxx"
+  api_key = "xxxx"
 }
 provider "aws" {
-  region = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
+  region = "ap-southeast-2"
+  access_key = "xxxx"
+  secret_key = "xxxx"
 }
 data "aws_route53_zone" "zone" {
   name = "securitytestingsolutions.com."
 }
-locals {
-  application_information = csvdecode(var.input_file)
-}
 
-resource "incapsula_site" "devops-sites" {
-    count = length(local.application_information)
-
-  domain                 = local.application_information[count.index].domain
-  account_id             = local.application_information[count.index].account_id
+resource "incapsula_site" "devops6-site" {
+  domain                 = "devops6.imperva.securitytestingsolutions.com"
+  account_id             = "1544595"
   ref_id                 = "123456"
-  force_ssl              = local.application_information[count.index].force_ssl
-  data_storage_region    = local.application_information[count.index].data_storage_region
-  send_site_setup_emails = local.application_information[count.index].send_site_setup_emails
-  site_ip                = local.application_information[count.index].site_ip
+  force_ssl              = "false"
+  data_storage_region    = "AU"
 }
-
-resource "aws_route53_record" "cert-validation-record" {
-
-  depends_on = [incapsula_site.devops-sites]
-  allow_overwrite = true
-  name = data.aws_route53_zone.zone.name
-  type = "TXT"
-  zone_id = data.aws_route53_zone.zone.zone_id
-  ttl = "60"
-  records = [incapsula_site.devops-sites[0].domain_verification]
-}
-
-resource "aws_route53_record" "cname-record" {
-    count = length(local.application_information)
-
-  depends_on = [incapsula_site.devops-sites]
-  allow_overwrite = true
-  name = incapsula_site.devops-sites[count.index].domain
+resource "aws_route53_record" "devops6-record" {
+  depends_on = [incapsula_site.devops6-site]
+  name = "devops6.imperva.${data.aws_route53_zone.zone.name}"
   type = "CNAME"
   zone_id = data.aws_route53_zone.zone.zone_id
   ttl = "60"
-  records = [incapsula_site.devops-sites[count.index].dns_cname_record_value!=null ? incapsula_site.devops-sites[count.index].dns_cname_record_value : 0]
+  records = [incapsula_site.devops6-site.dns_cname_record_value]
 }
 
-output "CNAME" {
-  value       = aws_route53_record.cert-validation-record.*.records
-}
--- variables.tf
+++
@@ -1,6 +0,0 @@
-variable "api_id" {}
-variable "api_key" {}
-variable "region" {}
-variable "access_key" {}
-variable "secret_key" {}
-variable "input_file" {}
